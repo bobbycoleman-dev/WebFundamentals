70e14d1729ea95b4c80b2e47897c4521494c46e6
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-07-12 11:04:43 -0500

    updates

--- Daily_Algos/day03/countPositives.js
+++ Daily_Algos/day03/countPositives.js
@@ -10,7 +10,7 @@ var numbers = [3, 4, -2, 7, 16, -8, 0];
 
 function countPositivesInArray (arrParam) {
     var countPositives = 0;
-    for(i=0; i<numbers.length; i++) {
+    for(i=0; i<arrParam.length; i++) {
         if (arrParam[i] > 0) {
             countPositives++
         }
