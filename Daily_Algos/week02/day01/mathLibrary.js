b2f00415a45f89d32f7d10ed3c7c2eeba5396298
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-07-17 11:00:16 -0500

    morning algos

---
+++ Daily_Algos/week02/day01/mathLibrary.js
@@ -0,0 +1,7 @@
+var floor = Math.floor(1.8); // 1
+var ceiling = Math.ceil(Math.PI); // 3.14...  // 4
+var random = Math.random(); // 0 - 0.99999...
+
+console.log(floor);
+console.log(ceiling);
+console.log(random);
