2de4636a27bb2a679cb3629432ff6673e1e8c2e6
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-07-16 19:04:03 -0500

    updated functionality in js for box clicker

--- JS/C_Button_Clicker/script.js
+++ JS/C_Button_Clicker/script.js
@@ -1,18 +1,31 @@
 // TODO: Change the text to say "Logout" when clicking the "Login" button
 function login(element) {
-    var login = false
+    var login = false;
     if (element.innerText == "Login") {
-        login = true
-        element.innerText = "Log Out"
+        login = true;
+        element.innerText = "Log Out";
     } else {
-        login = false
-        element.innerText = "Login"
+        login = false;
+        element.innerText = "Login";
     }
 }
 
 // TODO: Remove the Add Definition button when it is clicked
 function removeButton(element) {
-    element.remove()
+    element.remove();
 }
 
 // TODO: Alert "Ninja was liked" when clicking a like button. Done in HTML
+var likes1 = document.querySelector(".likes1");
+var likes2 = document.querySelector(".likes2");
+var count1 = 13;
+var count2 = 37;
+
+function increaseLikes1() {
+    count1++;
+    likes1.innerText = count1 + " likes";
+}
+function increaseLikes2() {
+    count2++;
+    likes2.innerText = count2 + " likes";
+}
