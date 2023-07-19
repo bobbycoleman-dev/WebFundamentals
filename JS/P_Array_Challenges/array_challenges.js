1c9a45d7d2c1e7bead20c12e32ccc08ab5778967
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-07-19 16:33:11 -0500

    array challenges completed

---
+++ JS/P_Array_Challenges/array_challenges.js
@@ -0,0 +1,74 @@
+// 1. Always Hungry
+function alwaysHungry(arr) {
+    if (arr.includes("food")) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] == "food") {
+                console.log("yummy");
+            }
+        }
+    } else {
+        console.log("I'm hungry");
+    }
+}
+
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
+
+// 2. High Pass Filter
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > cutoff) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+
+// 3. Better than average
+function betterThanAverage(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    sum /= arr.length;
+    var count = 0;
+    for (var j = 0; j < arr.length; j++) {
+        if (arr[j] > sum) {
+            count++;
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
+
+// 4. Array Reverse
+function reverse(arr) {
+    for (var i = 0; i < Math.floor(arr.length / 2); i++) {
+        [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]];
+    }
+    return arr;
+}
+
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+
+// 5. Fibonacci Array
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    var i = 0;
+    while (fibArr.length < n) {
+        fibArr.push(fibArr[i] + fibArr[i + 1]);
+        i++;
+    }
+    return fibArr;
+}
+
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
