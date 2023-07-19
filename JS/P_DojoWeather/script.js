32ae83659b5f03be639a056409d1e30dd9c18164
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-07-19 16:48:31 -0500

    updated dojoweather

--- JS/P_DojoWeather/script.js
+++ JS/P_DojoWeather/script.js
@@ -18,62 +18,78 @@ function convertToCel(fer) {
 }
 
 // Take in each day's max and min temp and either convert to celsius or fahrenheit based on selected element value
-function changeDegreeType(element) {
+function changeDegree(element) {
+    var maxTemp = document.querySelectorAll("#max");
+    var minTemp = document.querySelectorAll("#min");
     if (element.value == "fer") {
-        // Today
-        var max = document.getElementById("today-max");
-        max.innerText = convertToFer(max.innerText);
-
-        var min = document.getElementById("today-min");
-        min.innerText = convertToFer(min.innerText);
-
-        // Tomorrow
-        var max = document.getElementById("tom-max");
-        max.innerText = convertToFer(max.innerText);
-
-        var min = document.getElementById("tom-min");
-        min.innerText = convertToFer(min.innerText);
-
-        // Friday
-        var max = document.getElementById("fri-max");
-        max.innerText = convertToFer(max.innerText);
-
-        var min = document.getElementById("fri-min");
-        min.innerText = convertToFer(min.innerText);
-
-        // Saturday
-        var max = document.getElementById("sat-max");
-        max.innerText = convertToFer(max.innerText);
-
-        var min = document.getElementById("sat-min");
-        min.innerText = convertToFer(min.innerText);
+        for (var i = 0; i < maxTemp.length; i++) {
+            maxTemp[i].innerText = convertToFer(maxTemp[i].innerText);
+            minTemp[i].innerText = convertToFer(minTemp[i].innerText);
+        }
     } else {
-        // Today
-        var max = document.getElementById("today-max");
-        max.innerText = convertToCel(max.innerText);
-
-        var min = document.getElementById("today-min");
-        min.innerText = convertToCel(min.innerText);
-
-        // Tomorrow
-        var max = document.getElementById("tom-max");
-        max.innerText = convertToCel(max.innerText);
-
-        var min = document.getElementById("tom-min");
-        min.innerText = convertToCel(min.innerText);
-
-        // Friday
-        var max = document.getElementById("fri-max");
-        max.innerText = convertToCel(max.innerText);
-
-        var min = document.getElementById("fri-min");
-        min.innerText = convertToCel(min.innerText);
-
-        // Saturday
-        var max = document.getElementById("sat-max");
-        max.innerText = convertToCel(max.innerText);
-
-        var min = document.getElementById("sat-min");
-        min.innerText = convertToCel(min.innerText);
+        for (var i = 0; i < maxTemp.length; i++) {
+            maxTemp[i].innerText = convertToCel(maxTemp[i].innerText);
+            minTemp[i].innerText = convertToCel(minTemp[i].innerText);
+        }
     }
 }
+
+// function changeDegreeType(element) {
+//     if (element.value == "fer") {
+//         // Today
+//         var max = document.getElementById("today-max");
+//         max.innerText = convertToFer(max.innerText);
+
+//         var min = document.getElementById("today-min");
+//         min.innerText = convertToFer(min.innerText);
+
+//         // Tomorrow
+//         var max = document.getElementById("tom-max");
+//         max.innerText = convertToFer(max.innerText);
+
+//         var min = document.getElementById("tom-min");
+//         min.innerText = convertToFer(min.innerText);
+
+//         // Friday
+//         var max = document.getElementById("fri-max");
+//         max.innerText = convertToFer(max.innerText);
+
+//         var min = document.getElementById("fri-min");
+//         min.innerText = convertToFer(min.innerText);
+
+//         // Saturday
+//         var max = document.getElementById("sat-max");
+//         max.innerText = convertToFer(max.innerText);
+
+//         var min = document.getElementById("sat-min");
+//         min.innerText = convertToFer(min.innerText);
+//     } else {
+//         // Today
+//         var max = document.getElementById("today-max");
+//         max.innerText = convertToCel(max.innerText);
+
+//         var min = document.getElementById("today-min");
+//         min.innerText = convertToCel(min.innerText);
+
+//         // Tomorrow
+//         var max = document.getElementById("tom-max");
+//         max.innerText = convertToCel(max.innerText);
+
+//         var min = document.getElementById("tom-min");
+//         min.innerText = convertToCel(min.innerText);
+
+//         // Friday
+//         var max = document.getElementById("fri-max");
+//         max.innerText = convertToCel(max.innerText);
+
+//         var min = document.getElementById("fri-min");
+//         min.innerText = convertToCel(min.innerText);
+
+//         // Saturday
+//         var max = document.getElementById("sat-max");
+//         max.innerText = convertToCel(max.innerText);
+
+//         var min = document.getElementById("sat-min");
+//         min.innerText = convertToCel(min.innerText);
+//     }
+// }
