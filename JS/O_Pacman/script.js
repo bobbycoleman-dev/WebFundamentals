101388da832045cc020003718292b03af14ced42
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-07-21 09:32:52 -0500

    pacman updates

--- JS/O_Pacman/script.js
+++ JS/O_Pacman/script.js
@@ -123,12 +123,13 @@ var world = [
 ];
 
 var score = 0;
-
+var cherryCount = 0;
 var pacman = {
-    x: 1,
+    x: 2,
     y: 13,
 };
 
+// ! DISPLAY THE WORLD
 function displayWorld() {
     var output = "";
 
@@ -151,11 +152,25 @@ function displayWorld() {
     document.getElementById("world").innerHTML = output;
 }
 
+// ! DISPLAY PACMAN
 function displayPacman() {
     document.getElementById("pacman").style.top = pacman.y * 20 + "px";
     document.getElementById("pacman").style.left = pacman.x * 20 + "px";
+
+    if (pacman.y == 13 && pacman.x == 0) {
+        pacman.x = 27;
+        pacman.y = 13;
+        document.getElementById("pacman").style.top = pacman.y * 20 + "px";
+        document.getElementById("pacman").style.left = pacman.x * 20 + "px";
+    } else if (pacman.y == 13 && pacman.x == 27) {
+        pacman.x = 0;
+        pacman.y = 13;
+        document.getElementById("pacman").style.top = pacman.y * 20 + "px";
+        document.getElementById("pacman").style.left = pacman.x * 20 + "px";
+    }
 }
 
+// ! DISPLAY THE CHERRIES
 function displayCherry() {
     var pos1 = false;
     var pos2 = false;
@@ -200,6 +215,7 @@ function displayCherry() {
     }
 }
 
+// ! DISPLAY THE SCORE
 function displayScore() {
     document.getElementById("score").innerHTML = score;
 }
@@ -209,19 +225,23 @@ displayWorld();
 displayPacman();
 displayScore();
 
-var cherryCount = 0;
-
+// ! MOVEMENT / COLLISION DETECTION / COIN & CHERRY EATING
 document.onkeydown = function (e) {
     var output = "";
+    var pacmanDirection = document.getElementById("pacman");
 
     if (e.key == "ArrowRight" && world[pacman.y][pacman.x + 1] != 2) {
         pacman.x++;
+        pacmanDirection.style.transform = "rotate(360deg)";
     } else if (e.key == "ArrowLeft" && world[pacman.y][pacman.x - 1] != 2) {
         pacman.x--;
+        pacmanDirection.style.transform = "rotate(-180deg)";
     } else if (e.key == "ArrowUp" && world[pacman.y - 1][pacman.x] != 2) {
         pacman.y--;
+        pacmanDirection.style.transform = "rotate(-90deg)";
     } else if (e.key == "ArrowDown" && world[pacman.y + 1][pacman.x] != 2) {
         pacman.y++;
+        pacmanDirection.style.transform = "rotate(90deg)";
     }
 
     if (world[pacman.y][pacman.x] == 1) {
