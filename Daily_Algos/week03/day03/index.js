3805da40d23933c3c6052864cf32be884f8b9396
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-07-26 10:36:40 -0500

    daily algo

---
+++ Daily_Algos/week03/day03/index.js
@@ -0,0 +1,37 @@
+const arr1 = ["a", "b", "c", "d"];
+const idx1 = 1;
+const expected1 = "d";
+
+const arr2 = ["a", "b", "c", "d"];
+const idx2 = 2;
+const expected2 = "c";
+
+const arr3 = ["a", "b", "c", "d"];
+const idx3 = 0;
+const expected3 = null;
+
+const arr4 = ["a", "b", "c", "d"];
+const idx4 = -1;
+const expected4 = null;
+
+const arr5 = [];
+const idx5 = 2;
+const expected5 = null;
+
+function nthLast(items, nthToLast) {
+	var output = "";
+	if (items.length > 0) {
+		for (i = items.length - 1; i >= 0; i--) {
+			if (items[items.length - nthToLast] === undefined) {
+				output = null;
+			} else {
+				output = items[items.length - nthToLast];
+			}
+		}
+	} else {
+		output = null;
+	}
+	console.log(output);
+}
+
+nthLast(arr5, idx5);
